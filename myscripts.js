fca28a59970a9114a8a444476e712ba06d6f0ee1
UNIEKM <114246909+UNIEKM@users.noreply.github.com>
2022-11-23 21:45:54 -0500

    CSS for navbar + JS

--- myscripts.js
+++ myscripts.js
@@ -1,25 +1,12 @@
-// // Script for side navigation -- example change noon***COPIED FROM SHAWNS EXAMPLE
-// function w3_open() {
-//     var x = document.getElementById("mySidebar");
-//     x.style.width = "300px";
-//     x.style.paddingTop = "10%";
-//     x.style.display = "block";
-//   }
-  
-//   // Close side navigation
-//   function w3_close() {
-//     document.getElementById("mySidebar").style.display = "none";
-//   }
-  
-//   // Used to toggle the menu on smaller screens when clicking on the menu button
-//   function openNav() {
-//     var x = document.getElementById("navDemo");
-//     if (x.className.indexOf("w3-show") == -1) {
-//       x.className += " w3-show";
-//     } else { 
-//       x.className = x.className.replace(" w3-show", "");
-//     }
-//   }
+// ////---------------------------
+// * JavaScript/ Menu icon toggle/ Menu will appear/disappear on click with .active class in @media section 
+// * Inspired: W3S https://www.w3schools.com/jsref/met_element_addeventlistener.asp
+// * Owner : Kevin VD
+// * Date  : Nov 23 (v1)
+// -----------------------------//////
 
-
-//   //Getting started
+const toggleButton = document.getElementById('MenuIcon');//   Assign button to a variable "ID"
+const NavBarList = document.getElementById('NavList'); //Second variable for navbar list
+  toggleButton.addEventListener('click', function() {
+    NavBarList.classList.toggle('active');
+}) //Now we need an event listener for when user clicks the menu icon, once clicked, run function, selecting and toggle our active class for navlist (hidden in media screen settings)

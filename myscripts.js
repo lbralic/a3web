bc9b4aaeeb78686a3018cec492e3cc248d9e65c2
UNIEKM <114246909+UNIEKM@users.noreply.github.com>
2022-12-03 21:02:21 -0500

    Scroll To Top JavaScript Btn

--- myscripts.js
+++ myscripts.js
@@ -10,3 +10,26 @@ const NavBarList = document.getElementById('NavList'); //Second variable for nav
   toggleButton.addEventListener('click', function() {
     NavBarList.classList.toggle('active');
 }) //Now we need an event listener for when user clicks the menu icon, once clicked, run function, selecting and toggle our active class for navlist (hidden in media screen settings)
+
+//Scroll to top button: CREDIT: https://www.w3schools.com/howto/howto_js_scroll_to_top.asp 
+// Get the button by id "TopBtn":
+let mybutton = document.getElementById("TopBtn");
+
+// When the user scrolls down 300px from the top of the document, show the button.
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() { //Function to show or hide button depending position of page from top.
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+    mybutton.style.display = "flex"; //once page scrolls past 300px from the top button is displayed
+  } else {
+    mybutton.style.display = "none"; //before page scrolls past 300px from the top button is not displayed
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document using JavaScript topFunction()
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari **Not sure I need this but w3schools used it****
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera **Not sure I need this but w3schools used it****
+}
+
+ 
